f8e235630cc5d07220a09cc4f8fdf47cc37fa8d9
Brian Kim <gittybrian@gmail.com>
2022-02-25 16:40:23 -0500

    final commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,23 @@ const About = () => {
             exit={{ opacity: 0 }}
         >
             <div className='about-container'>
-                <h1>About</h1>
+                <h1>What is <b>contrast</b>?</h1>
+                <h3><b>contrast</b> is a diff tool that enables programmers
+                     to visualize two samples of text side by side and identify
+                     the textual differences between the two files.
+                </h3>
+                <h1>How do I use <b>contrast</b>?</h1>
+                <h3><b>1.</b> Start by pasting two samples of texts in the input areas.</h3>
+                <h3><b>2.</b> Submit the texts for diff viewing with the submit button.</h3>
+                <h3><b>3.</b> Visualize the differences in text with <b>contrast</b>'s diff vieweing interface.</h3>
+                <h3>After having submitted two samples of text
+                     the user will be prompted with a diff viewer:
+                </h3>
+                <h4>
+                     Lines that have been altered will be highlighted
+                     in red, while differences will be
+                     <u> underlined in green</u>.
+                </h4>
             </div>
         </motion.div>
     );
