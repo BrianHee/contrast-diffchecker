0d60530fdcacf8a0bea14df0a9ee57da6b4c832a
Brian Kim <gittybrian@gmail.com>
2022-06-21 22:44:48 -0400

    updated: component constants

--- src/constants/components.js
+++ src/constants/components.js
@@ -1,3 +1,21 @@
-const COMPONENTS = [];
+import Background from "../components/Background";
+
+const COMPONENTS = [
+    {
+        element: <Background />
+    },
+    {
+        element: <DiffEditor />
+    },
+    {
+        element: <DiffForm />
+    },
+    {
+        element: <Editor />
+    },
+    {
+        element: <Footer />
+    }
+];
 
 export default COMPONENTS;
