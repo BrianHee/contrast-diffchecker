9ddac0a52a63419d7aaba0c8d23eafbf79764c50
Brian Kim <gittybrian@gmail.com>
2024-04-19 14:05:57 -0400

    fixed: build warnings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import DiffForm from './components/DiffForm';
 import NavBar from'./components/NavBar';
 import { Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
-import About from './pages/About';
-import DiffEditor from './components/DiffEditor';
-import Error from './pages/Error';
 import Background from './components/Background';
-import Feedback from './pages/Feedback';
 import Footer from './components/Footer';
 
 import './index.css';
--- src/pages/About.js
+++ src/pages/About.js
@@ -10,16 +10,16 @@ const About = () => {
         >
             <div className='about-container'>
                 <h1>What is <b>contrast</b>?</h1>
-                <h3><b>contrast</b> is a diff tool that enables programmers
+                <h3><b>contrast</b> is a difference checking tool that enables programmers
                      to visualize two samples of text side by side and identify
                      the textual differences between the two files.
                 </h3>
                 <h1>How do I use <b>contrast</b>?</h1>
                 <h3><b>1.</b> Start by pasting two samples of texts in the input areas.</h3>
                 <h3><b>2.</b> Submit the texts for differences with the submit button.</h3>
-                <h3><b>3.</b> Visualize the differences in the texts with <b>contrast</b>'s diff viewing interface.</h3>
-                <h3>After having submitted two samples of text
-                     the user will be prompted with a diff viewer:
+                <h3><b>3.</b> Visualize the differences in the texts with <b>contrast</b>'s difference viewing interface.</h3>
+                <h3>After having submitted two samples of text,
+                     the user will be prompted with a difference viewer:
                 </h3>
                 <h4>
                      In the right pane, lines that have been altered will be highlighted
